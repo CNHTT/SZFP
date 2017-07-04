2a80384af7499a48027d8693cf96c58ce8ba6b23
cnhtt <cnhttt@163.com>
2017-07-04 23:18:07 +0800

    23：17

---
+++ app/src/main/java/com/szfp/szfp/view/activity/ParkingPayActivity.java
@@ -0,0 +1,15 @@
+package com.szfp.szfp.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.szfp.szfp.R;
+
+public class ParkingPayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parking_pay);
+    }
+}
